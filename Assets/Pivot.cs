39604981800f979b35cc9d81b0b10790e5995001
Daniel Bion Barreiros <bion@manifestohut.com>
2018-08-18 13:37:27 -0300

    - Minute setup

--- Assets/Pivot.cs
+++ Assets/Pivot.cs
@@ -10,8 +10,9 @@ public class Pivot : MonoBehaviour {
 	private const float hoursToDegrees = 360f/12f,
 	minutesToDegrees = 360f/60f,
 	secondsToDegrees= 6f;
+	private int offset = -90;
 	void Start () {
-		
+		transform.Rotate(new Vector3(0f, offset ,0f));
 	}
 	
 
@@ -20,6 +21,7 @@ public class Pivot : MonoBehaviour {
 	}
 	// Update is called once per frame
 	void Update () {
+		print(Mathf.Floor(((transform.eulerAngles.y - offset)/secondsToDegrees) % 60));
 		float step = speed * Time.deltaTime;
 		// var x = Input.GetAxis("Horizontal");
 		// var y = Input.GetAxis("Vertical");
