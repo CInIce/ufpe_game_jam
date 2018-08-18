f761befec364105e07d4b4106ce4333ecca8aa26
ltb <ltb@cin.ufpe.br>
2018-08-18 14:22:58 -0300

    correct hour pointer movement

--- Assets/Pivot.cs
+++ Assets/Pivot.cs
@@ -7,20 +7,25 @@ public class Pivot : MonoBehaviour {
 	// Use this for initialization
 	[SerializeField]
 	private float speed;
+	public bool isHour;
 	private const float hoursToDegrees = 360f/12f,
-	minutesToDegrees = 360f/60f,
-	secondsToDegrees= 6f;
+	secondsToDegrees= 360f/60f;
 	private int offset = -90;
 	void Start () {
+		
 		transform.Rotate(new Vector3(0f, offset ,0f));
 	}
 	
 
 	public void setPosition(bool isUp){
+		float offset = secondsToDegrees;
+		if(isHour){
+			offset = hoursToDegrees;
+		}
 		if(isUp){
-			transform.Rotate(new Vector3(0f, secondsToDegrees ,0f));
+			transform.Rotate(new Vector3(0f, offset ,0f));
 		}else{
-			transform.Rotate(new Vector3(0f, -secondsToDegrees ,0f));
+			transform.Rotate(new Vector3(0f, -offset ,0f));
 		}
 	}
 
@@ -29,7 +34,7 @@ public class Pivot : MonoBehaviour {
 	}
 	// Update is called once per frame
 	void Update () {
-		print(Mathf.Floor(((transform.eulerAngles.y - offset)/secondsToDegrees) % 60));
+		print(Mathf.Round(((transform.eulerAngles.y - offset)/secondsToDegrees) % 60));
 		float step = speed * Time.deltaTime;
 		// var x = Input.GetAxis("Horizontal");
 		// var y = Input.GetAxis("Vertical");
